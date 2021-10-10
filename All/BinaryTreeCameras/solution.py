558769b9210b3ca5f4cf419e86677c9f7dbd5c64
Shubham05178 <subhamnagaria@gmail.com>
2021-10-10 11:36:02 +0530

    Done

---
+++ All/BinaryTreeCameras/solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    cameras = 0
+
+    def minCameraCover(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode) -> int:
+            if not node: return 0
+            val = dfs(node.left) + dfs(node.right)
+            if val == 0: return 3
+            if val < 3: return 0
+            self.cameras += 1
+            return 1
+
+        return self.cameras + 1 if dfs(root) > 2 else self.cameras
