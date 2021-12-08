7d9d39f2087b2e23ad8698a5adb8029b268440f9
Shubham Nagaria <subhamnagaria@gmail.com>
2021-12-08 10:59:57 +0530

    Updated Methods of solving it

--- medium/Merge Intervals.cpp
+++ medium/Merge Intervals.cpp
@@ -26,3 +26,48 @@ public:
         return res;
     }
 };
+/**
+* Leetcode Problem - https://leetcode.com/problems/merge-intervals/
+* Author - Shubham Nagaria
+* Date - 8th Dec 2021
+Merge Intervals.cpp
+**/
+//Most optimal Approach -1
+// Time Complexity - O(N log N) and Space Complexity -O(N)
+class Solution {
+  public:
+    vector < vector < int >> merge(vector < vector < int >> & intervals) {
+      vector < vector < int >> mergeIntervals;
+      if (intervals.size() == 0)
+        return mergeIntervals;
+      sort(intervals.begin(), intervals.end());
+      vector < int > tempInterval = intervals[0];
+      for (auto it: intervals) {
+        if (it[0] <= tempInterval[1]) {
+          tempInterval[1] = max(it[1], tempInterval[1]);
+        } else {
+          mergeIntervals.push_back(tempInterval);
+          tempInterval = it;
+        }
+      }
+      mergeIntervals.push_back(tempInterval);
+      return mergeIntervals;
+    }
+};
+
+// Most Optimal Approach -2 
+// Time Complexity - O(N log N) Space Complexity O(N)
+class Solution {
+  public:
+    vector < vector < int >> merge(vector < vector < int >> & intervals) {
+      vector < vector < int >> mergeIntervals;
+      sort(intervals.begin(), intervals.end());
+      for (auto it: intervals) {
+        if (mergeIntervals.empty() || mergeIntervals.back()[1] < it[0])
+          mergeIntervals.push_back(it);
+        else
+          mergeIntervals.back()[1] = max(mergeIntervals.back()[1], it[1]);
+      }
+        return mergeIntervals;
+    }
+};
