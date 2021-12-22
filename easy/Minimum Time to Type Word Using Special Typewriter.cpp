aa9778820a04a941eae7d4ad39e0fb8d6998a5c6
Shubham Nagaria <subhamnagaria@gmail.com>
2021-12-22 11:12:29 +0530

    Minimum Time to Type Word Using Special Typewriter

---
+++ easy/Minimum Time to Type Word Using Special Typewriter.cpp
@@ -0,0 +1,20 @@
+/**
+Leetcode Problem - https://leetcode.com/contest/biweekly-contest-59/problems/minimum-time-to-type-word-using-special-typewriter/
+Author - Shubham Nagaria
+Date - 22nd Dec 2021
+**/
+class Solution {
+public:
+    int minTimeToType(string word) {
+        int time=word.size();
+        char prev='a';
+        for(char c:word){
+            if(c>=prev)
+                time+=min(c-prev,25-c+prev+1);
+            else
+                time+= min(prev-c,25-prev+c+1);
+            prev=c;
+        }
+    return time;
+    }
+};
