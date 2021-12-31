b814ba3e75f30674d905f16acf724a8a0647ddf8
Shubham Nagaria <subhamnagaria@gmail.com>
2022-01-01 03:04:01 +0530

    Group Anagrams

---
+++ medium/Group Anagrams.cpp
@@ -0,0 +1,51 @@
+/**
+* Leetcode Problem -  https://leetcode.com/problems/group-anagrams/
+* Author - Shubham Nagaria
+* Date - 1st Jan 2022
+**/
+//O(Nlog N) 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {    
+    unordered_map<string,vector<string>>m;
+    string s;
+    for(string i : strs){
+        s=i;
+        sort(s.begin(),s.end());
+        m[s].push_back(i);
+    }
+    vector<vector<string>> res;
+    for (auto i : m) res.push_back(i.second);
+             
+    return res;
+    }
+};
+
+//O(N) sorting because of count sort
+class Solution {
+public:
+    string countSort(string s){
+        int co[26]={0};
+        for(char ch: s)
+            co[ch-'a']++;
+        string t="";
+        for(int i =0; i <26; i++){
+            while(co[i]--) t+=(i+'a');            
+        }
+        return t;
+        
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {    
+    unordered_map<string,vector<string>>m;
+    string s;
+    for(string i : strs){
+        s=i;
+        
+        m[countSort(s)].push_back(i);
+    }
+    vector<vector<string>> res;
+    for (auto i : m) res.push_back(i.second);
+             
+    return res;
+    }
+};
